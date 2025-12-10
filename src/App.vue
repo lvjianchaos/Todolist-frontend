fa510a2f3109cd958dad60a755d7454bcab2ac8c
lvjianchaos <2277146577@qq.com>
2025-12-10 20:29:33 +0800

    feat: vue项目初始化

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+const handleClick = () => {
+  ElMessage.error('fuck')
+}
+</script>
+
+<template>
+  <el-button type="primary" @click="handleClick">Yes</el-button>
+  <el-icon>
+    <IEpSetting />
+  </el-icon>
+</template>
+
+<style scoped></style>
